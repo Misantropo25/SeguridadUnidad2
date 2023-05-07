82600c7a3aa8df047f67949e7fbd2c3052d26461
Jair Almanza <84415385+Misantropo25@users.noreply.github.com>
2023-05-07 18:09:55 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,67 +1,56 @@
 <?php
-session_start();
+// Conexión a la base de datos
+$host = "localhost";
+$username = "root";
+$password = "misan";
+$database = "SEGURIDAD";
+$conn = mysqli_connect($host, $username, $password, $database);
 
-// Verificar si el usuario ya ha iniciado sesión
-if (isset($_SESSION['usuario'])) {
-  header('Location: seguridad.php');
-  exit();
-}
+// Verificar si se envió el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
-$mensaje = '';
+  // Consultar si el usuario y la contraseña son correctos
+  $query = "SELECT * FROM usuarios WHERE nombre_usuario = '$username' AND contrasena = '$password'";
+  $result = mysqli_query($conn, $query);
 
-// Verificar si el formulario ha sido enviado
-if (isset($_POST['accion']) && $_POST['accion'] === 'iniciar-sesion') {
-  // Conectar a la base de datos
-  $conexion = mysqli_connect('localhost', 'root', 'misan', 'SEGURIDAD');
-
-  // Verificar si se pudo conectar a la base de datos
-  if (!$conexion) {
-    die('Error al conectar a la base de datos');
-  }
-
-  // Escapar los datos ingresados por el usuario para prevenir SQL injection
-  $usuario = mysqli_real_escape_string($conexion, $_POST['usuario']);
-  $contrasenia = mysqli_real_escape_string($conexion, $_POST['contrasenia']);
-
-  // Consultar la base de datos para verificar las credenciales del usuario
-  $consulta = "SELECT * FROM usuario WHERE usuario='$usuario' AND contrasenia='$contrasenia' LIMIT 1";
-  $resultado = mysqli_query($conexion, $consulta);
-
-  // Verificar si la consulta devolvió algún resultado
-  if (mysqli_num_rows($resultado) === 1) {
-    // Iniciar sesión y redirigir al usuario a la página de seguridad
-    $_SESSION['usuario'] = $usuario;
-    header('Location: seguridad.php');
-    exit();
+  // Si se encontró un resultado, redirigir al archivo seguridad.php
+  if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_assoc($result);
+    session_start();
+    $_SESSION["nombre_usuario"] = $row["nombre_usuario"];
+    header("Location: seguridad.php");
+    exit;
   } else {
-    // Mostrar mensaje de error si las credenciales son incorrectas
-    $mensaje = 'Usuario y/o contraseña incorrectos';
+    $error_message = "Usuario o contraseña incorrectos";
   }
-
-  // Cerrar la conexión a la base de datos
-  mysqli_close($conexion);
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Iniciar sesión</title>
-  <link rel="stylesheet" href="estilos.css">
+  <link rel="stylesheet" href="estilo.css">
+  <title>Inicio de sesion</title>
 </head>
 <body>
-  <div class="container">
-    <h1>Iniciar sesión</h1>
-    <form method="POST">
-      <input type="text" name="usuario" placeholder="Usuario" required>
-      <input type="password" name="contrasenia" placeholder="Contraseña" required>
-      <button type="submit">Iniciar sesión</button>
-      <?php if ($mensaje !== '') { ?>
-        <p><?php echo $mensaje; ?></p>
-      <?php } ?>
-      <input type="hidden" name="accion" value="iniciar-sesion">
-    </form>
-  </div>
+  <h1>Validar usuario</h1>
+
+  <?php if (isset($error_message)) { ?>
+    <p><?php echo $error_message; ?></p>
+  <?php } ?>
+
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="username">Usuario:</label>
+    <input type="text" id="username" name="username">
+
+    <label for="password">Contraseña:</label>
+    <input type="password" id="password" name="password">
+
+    <input type="submit" value="Iniciar sesión">
+  </form>
 </body>
 </html>
--- seguridad.php
+++ seguridad.php
@@ -1,41 +1,20 @@
 <?php
 session_start();
 
-// Verificar si el usuario ha iniciado sesión
-if (!isset($_SESSION['usuario'])) {
-  header('Location: index.php');
+if (!isset($_SESSION["nombre_usuario"])) {
+  header("Location: index.php");
   exit;
 }
 
-// Obtener la información del usuario
-// En este ejemplo, se utiliza información de ejemplo
-$nombre_usuario = 'Administrador';
-$email_usuario = 'admin@example.com';
-
-// Cerrar sesión si se ha enviado el formulario de deslogueo
-if (isset($_POST['accion']) && $_POST['accion'] == 'desloguear') {
-  session_destroy();
-  header('Location: index.php');
-  exit;
-}
+$nombre_usuario = $_SESSION["nombre_usuario"];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Información del usuario</title>
+  <title>Seguridad</title>
 </head>
 <body>
   <h1>Bienvenido, <?php echo $nombre_usuario; ?>!</h1>
-  <p>A continuación se muestra su información de usuario:</p>
-  <ul>
-    <li><strong>Nombre de usuario:</strong> <?php echo $nombre_usuario; ?></li>
-    <li><strong>Correo electrónico:</strong> <?php echo $email_usuario; ?></li>
-  </ul>
-  <p>Aquí puedes leer más información sobre seguridad en las bases de datos</p>
-  <p><a href="#" onclick="document.getElementById('formulario-deslogueo').submit();">Desloguearse</a></p>
-  <form id="formulario-deslogueo" method="POST">
-    <input type="hidden" name="accion" value="desloguear">
-  </form>
 </body>
 </html>
