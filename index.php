c4844b78a5ee67afcfed75bceda54058d5d7d652
Jair Almanza <84415385+Misantropo25@users.noreply.github.com>
2023-05-07 17:57:11 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,47 +1,67 @@
 <?php
 session_start();
 
-// Verificar si el usuario ya inició sesión
+// Verificar si el usuario ya ha iniciado sesión
 if (isset($_SESSION['usuario'])) {
   header('Location: seguridad.php');
-  exit;
+  exit();
 }
 
-// Verificar si se ha enviado el formulario
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $usuario = $_POST['usuario'];
-  $contrasenia = $_POST['contrasenia'];
+$mensaje = '';
 
-  // Verificar si el usuario y la contraseña son correctos
-  // En este ejemplo, se utiliza el usuario "admin" y la contraseña "12345"
-  if ($usuario == 'admin' && $contrasenia == '12345') {
+// Verificar si el formulario ha sido enviado
+if (isset($_POST['accion']) && $_POST['accion'] === 'iniciar-sesion') {
+  // Conectar a la base de datos
+  $conexion = mysqli_connect('localhost', 'root', 'misan', 'SEGURIDAD');
+
+  // Verificar si se pudo conectar a la base de datos
+  if (!$conexion) {
+    die('Error al conectar a la base de datos');
+  }
+
+  // Escapar los datos ingresados por el usuario para prevenir SQL injection
+  $usuario = mysqli_real_escape_string($conexion, $_POST['usuario']);
+  $contrasenia = mysqli_real_escape_string($conexion, $_POST['contrasenia']);
+
+  // Consultar la base de datos para verificar las credenciales del usuario
+  $consulta = "SELECT * FROM usuario WHERE usuario='$usuario' AND contrasenia='$contrasenia' LIMIT 1";
+  $resultado = mysqli_query($conexion, $consulta);
+
+  // Verificar si la consulta devolvió algún resultado
+  if (mysqli_num_rows($resultado) === 1) {
+    // Iniciar sesión y redirigir al usuario a la página de seguridad
     $_SESSION['usuario'] = $usuario;
     header('Location: seguridad.php');
-    exit;
+    exit();
   } else {
-    $mensaje_error = 'El usuario o la contraseña son incorrectos.';
+    // Mostrar mensaje de error si las credenciales son incorrectas
+    $mensaje = 'Usuario y/o contraseña incorrectos';
   }
+
+  // Cerrar la conexión a la base de datos
+  mysqli_close($conexion);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Formulario de acceso</title>
   <meta charset="UTF-8">
   <title>Iniciar sesión</title>
-  <link rel="stylesheet" href="estilo.css">
+  <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-  <?php if (isset($mensaje_error)): ?>
-    <div class="mensaje-error"><?php echo $mensaje_error; ?></div>
-  <?php endif; ?>
-  <form method="POST">
-    <label for="usuario">Usuario:</label>
-    <input type="text" name="usuario" required><br>
-    <label for="contrasenia">Contraseña:</label>
-    <input type="password" name="contrasenia" required><br>
-    <button type="submit">Acceder</button>
-  </form>
+  <div class="container">
+    <h1>Iniciar sesión</h1>
+    <form method="POST">
+      <input type="text" name="usuario" placeholder="Usuario" required>
+      <input type="password" name="contrasenia" placeholder="Contraseña" required>
+      <button type="submit">Iniciar sesión</button>
+      <?php if ($mensaje !== '') { ?>
+        <p><?php echo $mensaje; ?></p>
+      <?php } ?>
+      <input type="hidden" name="accion" value="iniciar-sesion">
+    </form>
+  </div>
 </body>
 </html>
