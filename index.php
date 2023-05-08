ab183fd001043cb508dce26d453437e4ae5f1c66
Jair Almanza <84415385+Misantropo25@users.noreply.github.com>
2023-05-07 21:19:59 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,80 +1,62 @@
 <?php
-  session_start();
-
-  // Si ya hay una sesión activa, redirige a seguridad.php
-  if(isset($_SESSION['id_usuario'])) {
+session_start();
+if(isset($_SESSION['id'])) {
     header("Location: seguridad.php");
     exit;
-  }
+}
 
-  $mensaje = "";
-  
-  // Verifica si se envió el formulario
-  if(isset($_POST['login'])) {
-    
-    // Conecta a la base de datos
-    $conexion = mysqli_connect("localhost", "jair", "misan", "SEGURIDAD");
-    
-    // Verifica si la conexión fue exitosa
+if(isset($_POST['submit'])) {
+    $servidor = "localhost";
+    $usuario_db = "jair";
+    $contraseña_db = "misan";
+    $nombre_db = "SEGURIDAD";
+    $conexion = mysqli_connect($servidor, $usuario_db, $contraseña_db, $nombre_db);
     if(mysqli_connect_errno()) {
-      $mensaje = "Error de conexión a la base de datos: " . mysqli_connect_error();
+        $mensaje = "Error de conexión a la base de datos: " . mysqli_connect_error();
     } else {
-    
-      // Obtiene los datos ingresados por el usuario
-      $usuario = $_POST['usuario'];
-      $contraseña = $_POST['contraseña'];
-    
-      // Verifica si los datos son correctos
-      $query = "SELECT id, usuario FROM usuarios WHERE usuario = '$usuario' AND contrasenia = '$contraseña'";
-      $resultado = mysqli_query($conexion, $query);
-      
-      if(mysqli_num_rows($resultado) == 1) {
-        
-        // Inicia la sesión y almacena los datos del usuario
-        $fila = mysqli_fetch_assoc($resultado);
-        $_SESSION['id_usuario'] = $fila['id'];
-        $_SESSION['nombre_usuario'] = $fila['usuario'];
-        
-        // Redirige a la página segura
-        header("Location: seguridad.php");
-        exit;
-        
-      } else {
-        $mensaje = "Nombre de usuario o contraseña incorrectos";
-      }
+        $usuario = mysqli_real_escape_string($conexion, $_POST['usuario']);
+        $contrasenia = mysqli_real_escape_string($conexion, $_POST['contrasenia']);
+        $query = "SELECT id, usuario FROM usuarios WHERE usuario='$usuario' AND contrasenia='$contrasenia'";
+        $resultado = mysqli_query($conexion, $query);
+        if(mysqli_num_rows($resultado) == 1) {
+            $fila = mysqli_fetch_assoc($resultado);
+            $_SESSION['id'] = $fila['id'];
+            $_SESSION['usuario'] = $fila['usuario'];
+            header("Location: seguridad.php");
+            exit;
+        } else {
+            $mensaje = "Nombre de usuario o contraseña incorrectos";
+        }
     }
-  }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>Iniciar sesión</title>
-  <link rel="stylesheet" href="estilos.css">
+    <meta charset="utf-8">
+    <title>Inicio de sesión</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
 
-  <h1>Iniciar sesión</h1>
-  
-  <?php
-    // Muestra el mensaje de error o éxito
-    if($mensaje != "") {
-      echo "<p class='mensaje'>" . $mensaje . "</p>";
-    }
-  ?>
-  
-  <form method="post" action="index.php">
+    <h1>Iniciar sesión</h1>
 
-    <label for="usuario">Usuario:</label>
-    <input type="text" id="usuario" name="usuario" required>
+    <?php if(isset($mensaje)): ?>
+        <p class="mensaje"><?php echo $mensaje; ?></p>
+    <?php endif; ?>
 
-    <label for="contraseña">Contraseña:</label>
-    <input type="password" id="contraseña" name="contraseña" required>
+    <form method="POST" action="index.php">
 
-    <button type="submit" name="login">Iniciar sesión</button>
+        <label for="usuario">Usuario:</label>
+        <input type="text" id="usuario" name="usuario" required>
 
-  </form>
+        <label for="contrasenia">Contraseña:</label>
+        <input type="password" id="contrasenia" name="contrasenia" required>
+
+        <button type="submit" name="submit">Iniciar sesión</button>
+
+    </form>
 
 </body>
 </html>
