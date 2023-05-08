e6a3dcffc6f936b97893ec31a4c66196f3e08fcd
Jair Almanza <84415385+Misantropo25@users.noreply.github.com>
2023-05-07 21:13:50 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,82 +1,78 @@
 <?php
-session_start(); // Iniciamos una sesión para almacenar los datos del usuario
+  session_start();
 
-// Si el usuario ya está logueado, lo redirigimos a la página de seguridad
-if(isset($_SESSION['id_usuario'])) {
-  header("Location: seguridad.php");
-  exit();
-}
-
-// Verificamos si el formulario ha sido enviado
-if(isset($_POST['submit'])) {
-
-  // Conectamos a la base de datos
-  $conexion = mysqli_connect('localhost', 'root', 'misan', 'SEGURIDAD');
-
-  // Verificamos si la conexión fue exitosa
-  if(!$conexion) {
-    die("Error al conectar con la base de datos: " . mysqli_connect_error());
-  }
-
-  // Obtenemos los datos del formulario
-  $usuario = $_POST['usuario'];
-  $contraseña = $_POST['contraseña'];
-
-  // Preparamos la consulta para verificar si el usuario existe en la base de datos
-  $consulta = "SELECT id, nombre FROM usuarios WHERE usuario='$usuario' AND contraseña='$contraseña'";
-
-  // Ejecutamos la consulta
-  $resultado = mysqli_query($conexion, $consulta);
-
-  // Verificamos si se encontró un usuario con los datos ingresados
-  if(mysqli_num_rows($resultado) == 1) {
-
-    // Almacenamos los datos del usuario en la sesión
-    $fila = mysqli_fetch_assoc($resultado);
-    $_SESSION['id_usuario'] = $fila['id'];
-    $_SESSION['nombre_usuario'] = $fila['nombre'];
-
-    // Redirigimos al usuario a la página de seguridad
+  // Si ya hay una sesión activa, redirige a seguridad.php
+  if(isset($_SESSION['id_usuario'])) {
     header("Location: seguridad.php");
-    exit();
-
-  } else {
-
-    // Mostramos un mensaje de error si no se encontró un usuario con los datos ingresados
-    $mensaje = "Usuario o contraseña incorrectos.";
-
+    exit;
   }
 
-  // Cerramos la conexión a la base de datos
-  mysqli_close($conexion);
-}
-
+  $mensaje = "";
+  
+  // Verifica si se envió el formulario
+  if(isset($_POST['login'])) {
+    
+    // Conecta a la base de datos
+    $conexion = mysqli_connect("localhost", "jair", "misan", "SEGURIDAD");
+    
+    // Verifica si la conexión fue exitosa
+    if(mysqli_connect_errno()) {
+      $mensaje = "Error de conexión a la base de datos: " . mysqli_connect_error();
+    } else {
+    
+      // Obtiene los datos ingresados por el usuario
+      $usuario = $_POST['usuario'];
+      $contraseña = $_POST['contraseña'];
+    
+      // Verifica si los datos son correctos
+      $query = "SELECT id, usuario FROM usuarios WHERE usuario = '$usuario' AND contrasenia = '$contraseña'";
+      $resultado = mysqli_query($conexion, $query);
+      
+      if(mysqli_num_rows($resultado) == 1) {
+        
+        // Inicia la sesión y almacena los datos del usuario
+        $fila = mysqli_fetch_assoc($resultado);
+        $_SESSION['id_usuario'] = $fila['id'];
+        $_SESSION['nombre_usuario'] = $fila['usuario'];
+        
+        // Redirige a la página segura
+        header("Location: seguridad.php");
+        exit;
+        
+      } else {
+        $mensaje = "Nombre de usuario o contraseña incorrectos";
+      }
+    }
+  }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Formulario de acceso</title>
-  <link rel="stylesheet" href="estilo.css">
+  <title>Iniciar sesión</title>
+  <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
 
-  <h1>Formulario de acceso</h1>
-
-  <?php if(isset($mensaje)) { ?>
-    <p><?php echo $mensaje; ?></p>
-  <?php } ?>
-
+  <h1>Iniciar sesión</h1>
+  
+  <?php
+    // Muestra el mensaje de error o éxito
+    if($mensaje != "") {
+      echo "<p class='mensaje'>" . $mensaje . "</p>";
+    }
+  ?>
+  
   <form method="post" action="index.php">
 
     <label for="usuario">Usuario:</label>
-    <input type="text" name="usuario" required>
+    <input type="text" id="usuario" name="usuario" required>
 
     <label for="contraseña">Contraseña:</label>
-    <input type="password" name="contraseña" required>
+    <input type="password" id="contraseña" name="contraseña" required>
 
-    <button type="submit" name="submit">Acceder</button>
+    <button type="submit" name="login">Iniciar sesión</button>
 
   </form>
 
